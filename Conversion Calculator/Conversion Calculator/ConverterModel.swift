da364fc02d550a15832d27d4b4bf15e6c8b27015
Me <cmwg3c@mail.missouri.edu>
2018-05-07 21:38:25 -0500

    Made output round to 2 decimal places

--- Conversion Calculator/Conversion Calculator/ConverterModel.swift
+++ Conversion Calculator/Conversion Calculator/ConverterModel.swift
@@ -70,7 +70,7 @@ class Conversion {
         outputValue = convertUnit(input: inputValue)
         
         inputDisplay = inputString + converter.inputUnit
-        outputDisplay = "\(outputValue)" + converter.outputUnit
+        outputDisplay = String(format: "%.2f", outputValue) + converter.outputUnit
         
     }
     
